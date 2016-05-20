7ed72ff13a5097eae7cfb2d0870946da7742ae1f
Daniel Brain <dbrain@paypal.com>
2016-05-20 11:22:00 -0700

    First commit

---
+++ src/promise.js
@@ -0,0 +1,109 @@
+
+var SyncPromise = function SyncPromise(handler) {
+
+    this.resolved = false;
+    this.rejected = false;
+
+    this.handlers = [];
+
+    if (!handler) {
+        return;
+    }
+
+    var self = this;
+
+    handler(function resolver(result) {
+        self.resolve(result);
+    }, function rejector(error) {
+        self.reject(error);
+    });
+};
+
+SyncPromise.resolve = function(value) {
+    return new SyncPromise().resolve(value);
+};
+
+SyncPromise.reject = function(error) {
+    return new SyncPromise().reject(error);
+};
+
+SyncPromise.prototype.resolve = function(result) {
+    if (this.resolved || this.rejected) {
+        return this;
+    };
+    this.resolved = true;
+    this.value = result;
+    this.dispatch();
+    return this;
+};
+
+SyncPromise.prototype.reject = function(error) {
+    if (this.resolved || this.rejected) {
+        return this;
+    };
+    this.rejected = true;
+    this.value = error;
+    this.dispatch();
+    return this;
+};
+
+SyncPromise.prototype.dispatch = function() {
+
+    if (!this.resolved && !this.rejected) {
+        return;
+    }
+
+    while (this.handlers.length) {
+
+        var handler = this.handlers.shift();
+
+        var result, error;
+
+        try {
+            if (this.resolved) {
+                result = handler.onSuccess ? handler.onSuccess(this.value) : this;
+            } else {
+                result = handler.onError ? handler.onError(this.value) : this;
+            }
+        } catch (err) {
+            console.log(err.stack || err.toString());
+            error = err;
+        }
+
+        if (error) {
+            handler.promise.reject(error);
+
+        } else if (result instanceof SyncPromise) {
+            result.then(res => handler.promise.resolve(res),
+                        err => handler.promise.reject(err));
+
+        } else {
+            handler.promise.resolve(result)
+        }
+    }
+
+};
+
+SyncPromise.prototype.then = function(onSuccess, onError) {
+
+    var promise = new SyncPromise();
+
+    this.handlers.push({
+        promise: promise,
+        onSuccess: onSuccess,
+        onError: onError
+    });
+
+    this.dispatch();
+
+    return promise;
+};
+
+SyncPromise.prototype.catch = function(onError) {
+    return this.then(null, onError);
+};
+
+
+export function patchPromise() {
+    window.Promise = SyncPromise;
+}
---
+++ src/xhr.js
@@ -0,0 +1,203 @@
+
+var endpoints = [];
+
+export function $mockEndpoint(options) {
+
+    options.method = options.method || 'GET';
+
+    if (typeof options.uri === 'string') {
+        options.uri = options.uri.replace(/\?/g, '\\?');
+    }
+
+    this.status = options.status || 200;
+
+    this.rawMethod = options.method.toString();
+    this.rawUri = options.uri.toString();
+
+    this.method = (options.method instanceof RegExp) ? options.method : new RegExp(options.method, 'i');
+    this.uri = (options.uri instanceof RegExp) ? options.uri : new RegExp(options.uri, 'i');
+
+    this.handler = options.handler;
+    this.data = options.data;
+
+    endpoints.unshift(this);
+}
+
+$mockEndpoint.prototype = {
+
+    enabled: false,
+    expect: false,
+    called: false,
+
+    listen: function() {
+        this.listening = true;
+        this.enable();
+        return this;
+    },
+
+    enable() {
+        this.enabled = true;
+        return this;
+    },
+
+    disable() {
+        this.enabled = false;
+        this.expect = false;
+        return this;
+    },
+
+    match(method, uri) {
+        return Boolean(this.method.test(method) && this.uri.test(uri));
+    },
+
+    call(data, params) {
+
+        this.called = true;
+
+        if (this.handler) {
+            return this.handler(data, params);
+        }
+
+        return this.data;
+    },
+
+    expectCalls: function() {
+
+        endpoints.splice(endpoints.indexOf(this), 1);
+        endpoints.unshift(this);
+
+        this.expect = true;
+        this.called = false;
+
+        this.enable();
+        return this;
+    },
+
+    done() {
+
+        var notCalled = this.expect && !this.called;
+
+        if (!this.listening) {
+            this.disable();
+        }
+
+        this.expect = false;
+        this.called = false;
+
+        if (notCalled) {
+            throw new Error(`Expected call to ${this.rawMethod} ${this.rawUri}`);
+        }
+    }
+};
+
+
+$mockEndpoint.register = function(options) {
+    return new $mockEndpoint(options);
+};
+
+$mockEndpoint.find = function(method, uri) {
+
+    var match;
+
+    endpoints.some(endpoint => {
+        if (endpoint.match(method, uri) && endpoint.enabled) {
+            match = endpoint;
+            return match;
+        }
+    });
+
+    return match;
+};
+
+export function patchXmlHttpRequest() {
+
+    window.XMLHttpRequest = function() {
+
+    };
+
+    window.XMLHttpRequest.prototype = {
+
+        listen: function() {
+
+        },
+
+        open: function(method, uri) {
+            this.method = method;
+            this.uri = uri;
+            this.mock = $mockEndpoint.find(method, uri);
+            this.params = {};
+            var params = uri.indexOf('?') >= 0 ? uri.substr(uri.indexOf('?') + 1).split('&') : [];
+            params.forEach((param) => {
+                var temp = param.split('=');
+                this.params[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
+            });
+
+            if (!this.mock) {
+                throw new Error(`Unexpected ${method.toUpperCase()} ${uri}`);
+            }
+        },
+
+        setRequestHeader: function() {
+
+        },
+
+        getResponseHeader: function() {
+
+        },
+
+        getAllResponseHeaders: function() {
+            return 'Content-Type: application/json';
+        },
+
+        send: function(data) {
+
+            if (data) {
+                data = JSON.parse(data);
+            }
+
+            console.debug('REQUEST', this.method, this.uri, data);
+
+            var response = this.mock.call(data, this.params);
+
+            this._respond(this.mock.status, response);
+
+            let callback;
+
+            let descriptor = {
+                get: () => {
+                    return callback;
+                },
+                set: (value) => {
+                    callback = value;
+                    this._respond(this.mock.status, response);
+                }
+            };
+
+            Object.defineProperty(this, 'onreadystatechange', descriptor);
+            Object.defineProperty(this, 'onload', descriptor);
+        },
+
+        _respond(status, response) {
+
+            if (this._responded) {
+                return;
+            }
+
+            let callback = this.onreadystatechange || this.onload;
+
+            if (!callback) {
+                return;
+            }
+
+            this._responded = true;
+
+            console.debug('RESPONSE', status, JSON.stringify(response, 0, 2));
+
+            this.status = status;
+            this.response = this.responseText = JSON.stringify(response);
+            this.readyState = 4;
+
+            callback.call(this);
+        }
+    };
+}
