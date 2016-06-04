685c1a462e8eb20defdd99493ebbf14759dcc1cd
Daniel Brain <daniel@bluesuncorp.co.uk>
2016-06-03 23:06:59 -0700

    Use latest promise code

--- src/promise.js
+++ src/promise.js
@@ -1,4 +1,40 @@
 
+function trycatch(method, successHandler, errorHandler) {
+
+    var isCalled = false;
+    var isSuccess = false;
+    var isError = false;
+    var err, res;
+
+    function flush() {
+        if (isCalled) {
+            if (isError) {
+                return errorHandler(err);
+            } else if (isSuccess) {
+                return successHandler(res);
+            }
+        }
+    }
+
+    try {
+        method(function(result) {
+            res = result;
+            isSuccess = true;
+            flush();
+        }, function(error) {
+            err = error;
+            isError = true;
+            flush();
+        });
+    } catch (error) {
+        return errorHandler(error);
+    }
+
+    isCalled = true;
+    flush();
+}
+
+
 export var SyncPromise = function SyncPromise(handler) {
 
     this.resolved = false;
@@ -12,24 +48,33 @@ export var SyncPromise = function SyncPromise(handler) {
 
     var self = this;
 
-    handler(function resolver(result) {
-        self.resolve(result);
-    }, function rejector(error) {
-        self.reject(error);
+    trycatch(handler, function(res) {
+        return self.resolve(res);
+    }, function(err) {
+        return self.reject(err);
     });
 };
 
-SyncPromise.resolve = function(value) {
+SyncPromise.resolve = function SyncPromiseResolve(value) {
+
+    if (value && value.then) {
+        return value;
+    }
+
     return new SyncPromise().resolve(value);
 };
 
-SyncPromise.reject = function(error) {
+SyncPromise.reject = function SyncPromiseResolve(error) {
     return new SyncPromise().reject(error);
 };
 
-SyncPromise.prototype.resolve = function(result) {
+SyncPromise.prototype.resolve = function (result) {
     if (this.resolved || this.rejected) {
         return this;
+    }
+
+    if (result && result.then) {
+        throw new Error('Can not resolve promise with another promise');
     }
 
     this.resolved = true;
@@ -42,6 +87,10 @@ SyncPromise.prototype.resolve = function(result) {
 SyncPromise.prototype.reject = function(error) {
     if (this.resolved || this.rejected) {
         return this;
+    }
+
+    if (error && error.then) {
+        throw new Error('Can not reject promise with another promise');
     }
 
     this.rejected = true;
@@ -65,27 +114,33 @@ SyncPromise.prototype.dispatch = function() {
 
         try {
             if (this.resolved) {
-                result = handler.onSuccess ? handler.onSuccess(this.value) : this;
+                result = handler.onSuccess ? handler.onSuccess(this.value) : this.value;
             } else {
-                result = handler.onError ? handler.onError(this.value) : this;
+                if (handler.onError) {
+                    result = handler.onError(this.value);
+                } else {
+                    error = this.value;
+                }
             }
         } catch (err) {
-            console.log(err.stack || err.toString());
             error = err;
+        }
+
+        if (result === this) {
+            throw new Error('Can not return a promise from the the same promise');
         }
 
         if (error) {
             handler.promise.reject(error);
 
-        } else if (result instanceof SyncPromise) {
-            result.then(res => handler.promise.resolve(res),
-                        err => handler.promise.reject(err));
+        } else if (result && result.then) {
+            result.then(res => { handler.promise.resolve(res); },
+                    err => { handler.promise.reject(err); });
 
         } else {
             handler.promise.resolve(result);
         }
     }
-
 };
 
 SyncPromise.prototype.then = function(onSuccess, onError) {
@@ -105,6 +160,33 @@ SyncPromise.prototype.then = function(onSuccess, onError) {
 
 SyncPromise.prototype.catch = function(onError) {
     return this.then(null, onError);
+};
+
+SyncPromise.prototype.done = function(successHandler, errorHandler) {
+    this.then(successHandler, errorHandler || function(err) {
+            console.error(err.stack || err.toString());
+        });
+};
+
+SyncPromise.all = function(promises) {
+
+    var promise = new SyncPromise();
+    var count = promises.length;
+    var results = [];
+
+    for (var i = 0; i < promises.length; i++) {
+        promises[i].then(function(result) {
+            results[i] = result;
+            count -= 1;
+            if (count === 0) {
+                promise.resolve(results);
+            }
+        }, function(err) {
+            promise.reject(err);
+        });
+    }
+
+    return promise;
 };
 
 
