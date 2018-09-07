115195dd8e0a8acf174c675feed66d360f3b28d7
Daniel Brain <dbrain@paypal.com>
2018-09-07 16:14:08 -0700

    Remove sync-promise (use zalgo-promise instead)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 
-import { patchPostMessage } from './postMessage';
-import { patchPromise } from './promise';
 import { patchSetTimeout, patchSetInterval } from './timeout';
 import { patchXmlHttpRequest } from './xhr';
 
 export function patchAll() {
-    patchPostMessage();
-    patchPromise();
     patchSetTimeout();
     patchSetInterval();
     patchXmlHttpRequest();
 }
 
-export * from './postMessage';
-export * from './promise';
 export * from './timeout';
 export * from './xhr';
