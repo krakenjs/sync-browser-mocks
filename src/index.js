754eb363b3afeaa26b9a85f92f34bc3edd2a4317
Daniel Brain <daniel@brainblocks.io>
2019-09-09 21:42:03 -0700

    Add websocket mock

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 import { patchSetTimeout, patchSetInterval } from './timeout';
 import { patchXmlHttpRequest } from './xhr';
+import { patchWebSocket } from './websocket';
 
 export function patchAll() {
     patchSetTimeout();
     patchSetInterval();
     patchXmlHttpRequest();
+    patchWebSocket();
 }
 
 export * from './timeout';
 export * from './xhr';
+export * from './webSocket';
---
+++ src/webSocket.js
@@ -0,0 +1,77 @@
+
+const mockWebSockets = [];
+
+export function mockWebSocket({ uri, handler }) {
+    let listening = false;
+    let hasCalls = false;
+
+    const mock = {
+        isListening: (mockUri) => {
+            return (listening && mockUri === uri);
+        },
+        listen: () => {
+            listening = true;
+            return {
+                cancel: () => {
+                    listening = false;
+                }
+            };
+        },
+        receive: ({ data: receiveData, socket }) => {
+            hasCalls = true;
+
+            handler({
+                data: receiveData,
+                send: (sendData) => {
+                    if (socket.onmessage) {
+                        socket.onmessage({
+                            data: sendData
+                        });
+                    }
+                }
+            });
+        },
+        expect: () => {
+            listening = true;
+            return {
+                done: () => {
+                    listening = false;
+                    if (!hasCalls) {
+                        throw new Error('Expected websocket calls');
+                    }
+                }
+            };
+        }
+    };
+
+    mockWebSockets.push(mock);
+
+    return mock;
+}
+
+function SyncWebSocket(socketUri) {
+    const socket = {
+        send: (data) => {
+            for (let i = (mockWebSockets.length - 1); i >= 0; i--) {
+                const mock = mockWebSockets[i];
+
+                if (mock.isListening(socketUri)) {
+                    mock.receive({ data, socket });
+                    return;
+                }
+            }
+        }
+    };
+
+    setTimeout(() => {
+        if (socket.onopen) {
+            socket.onopen();
+        }
+    });
+
+    return socket;
+}
+
+export function patchWebSocket() {
+    window.WebSocket = SyncWebSocket;
+}
